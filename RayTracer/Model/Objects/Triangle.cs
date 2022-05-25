ea971f992e0c95765d808078ee4e0525882fc996
Cárina Alas <carina.alas@farfetch.com>
2022-05-25 22:07:05 +0100

    Normalizing triangles

--- RayTracer/Model/Objects/Triangle.cs
+++ RayTracer/Model/Objects/Triangle.cs
@@ -1,14 +1,15 @@
 ﻿namespace RayTracer.Model.Objects
 {
+    using System;
     using System.Numerics;
 
     public class Triangle : Object3D
     {
-        public Vector3 V0 { get; set; }
+        public Vector3 P0 { get; set; }
 
-        public Vector3 V1 { get; set; }
+        public Vector3 P1 { get; set; }
 
-        public Vector3 V2 { get; set; }
+        public Vector3 P2 { get; set; }
 
         public Vector3 Normal { get; set; }
 
@@ -16,9 +17,28 @@
         {
             this.Transformation = transformation;
             this.Material = material;
-            this.V0 = v0;
-            this.V1 = v1;
-            this.V2= v2;
+            this.P0 = v0;
+            this.P1 = v1;
+            this.P2= v2;
+            this.Normal = this.NormalizeTriangle(v0,v1,v2);
+        }
+
+        private Vector3 NormalizeTriangle(Vector3 v0, Vector3 v1, Vector3 v2)
+        {
+            var newV1 = Vector3.Subtract(v1, v0);
+            var newV2 = Vector3.Subtract(v2, v0);
+
+            var newX = ((newV1.Y * newV2.Z) - (newV1.Z * newV2.Y));
+            var newY = ((newV1.Z * newV2.X) - (newV1.X * newV2.Z));
+            var newZ = ((newV1.X * newV2.Y) - (newV1.Y * newV2.X));
+            var size = Math.Sqrt(Math.Pow(newX, 2) + Math.Pow(newY, 2) + Math.Pow(newZ, 2));
+
+            return new Vector3
+            {
+                X = (float)(newX / size),
+                Y = (float)(newY / size),
+                Z = (float)(newZ / size)
+            };
         }
     }
 }
